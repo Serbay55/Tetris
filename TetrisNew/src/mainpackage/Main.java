b9269e97a6c5f0395666a6a8fc080f8329272993
Tobias.Schwarz <Tobias.Schwarz@W10-SchwarzT.ps.psb-gmbh.de>
2022-04-14 14:03:13 +0200

    Hello there

--- TetrisNew/src/mainpackage/Main.java
+++ TetrisNew/src/mainpackage/Main.java
@@ -3,7 +3,7 @@ package mainpackage;
 public class Main {
     public static void main(String[] args){
         System.out.println("test");
-        System.out.println("Hello");
+        System.out.println("Hello There");
         System.out.println("OH? YOU'RE APPROACHING ME?");
     }
 }
