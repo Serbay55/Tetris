aaeac8da928ee8cfa04bd6e192142446e9cf2a38
Lev Likhanov <lev.likhanov@yahoo.de>
2022-04-14 13:58:13 +0200

    testing

--- TetrisNew/src/mainpackage/Main.java
+++ TetrisNew/src/mainpackage/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("test");
         System.out.println("Hello");
-        System.out.println("JOTARO!");
+        System.out.println("OH? YOU'RE APPROACHING ME?");
     }
 }
