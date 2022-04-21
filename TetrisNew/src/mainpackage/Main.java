ee9ffc1607b9b5e2dcb814cd417c7e69bbb09f05
Serbay Yavuz <xxzmk@outlook.de>
2022-04-21 10:15:19 +0200

    test

--- TetrisNew/src/mainpackage/Main.java
+++ TetrisNew/src/mainpackage/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("test");
         System.out.println("Hello There");
         System.out.println("OH? YOU'RE APPROACHING ME?");
-        System.out.println("Good Morning!");
+        System.out.println("Good Morning! <--- whoever that was broke it");
     }
 }
