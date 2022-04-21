5916cc9b2a21c5fc52d0df6535e67f8d655321de
Serbay Yavuz <xxzmk@outlook.de>
2022-04-21 11:49:16 +0200

    1

--- TetrisNew/src/mainpackage/Main.java
+++ TetrisNew/src/mainpackage/Main.java
@@ -1,10 +1,15 @@
 package mainpackage;
 
+import ui.Gui;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("test");
         System.out.println("Hello There");
         System.out.println("OH? YOU'RE APPROACHING ME?");
         System.out.println("Good Morning! <--- whoever that was broke it");
+        
+        Gui g = new Gui();
+        g.create();
     }
 }
