d741e9e1dd3895a5109772f7bedba2febb114ed6
Serbay Yavuz <xxzmk@outlook.de>
2022-04-21 11:04:21 +0200

    Die ersten Schritte

---
+++ TetrisNew/src/ui/Gui.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class Gui {
+	
+	public static int width = 320, height = 576;
+	
+	JFrame jf;
+	
+	public void create() {
+		jf = new JFrame();
+		jf.setSize(width + 17 + 200, height + 41);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setLocationRelativeTo(null);
+		jf.setResizable(false);
+		jf.setLayout(null);
+		jf.requestFocus();
+	}
+
+}
