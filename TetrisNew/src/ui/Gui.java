1348c61e9c86a617ffc60bfe3583e767ec648627
Lev Likhanov <lev.likhanov@yahoo.de>
2022-05-05 09:43:45 +0200

    fixed Button positioning

--- TetrisNew/src/ui/Gui.java
+++ TetrisNew/src/ui/Gui.java
@@ -23,13 +23,13 @@ public class Gui {
 		jf.addKeyListener(new KeyHandler());
 		jf.requestFocus();
 		JButton button1=new JButton("Start");   
-		button1.setBounds(350,60,120,30);
+		button1.setBounds(350,160,120,30);
 	    jf.add(button1);  
 	    JButton button2=new JButton("Stop");   
-	    button2.setBounds(350,120,120,30);
+	    button2.setBounds(350,220,120,30);
 	    jf.add(button2);  
 	    JButton button3=new JButton("Neustart");   
-	    button3.setBounds(350,180,120,30);
+	    button3.setBounds(350,280,120,30);
 	    jf.add(button3);  
 		
 		jf.setVisible(true);
